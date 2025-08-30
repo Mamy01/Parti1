46e1145facd9a9f45c0177a27b9d22c940e53d3c
Youmane KA <youmaneka00@gmail.com>
2025-08-30 21:39:55 +0000

    exercise 1.4

--- courseinfo/src/Content.jsx
+++ courseinfo/src/Content.jsx
@@ -1,20 +1,26 @@
 import Part from "./Part.jsx"
 
-function Content (){
-  const part1 = {
-    name:'Fundamentals of React',
-    exercises: 10}
-  const part2 ={
-    name: 'Using props to pass data',
-    exercises: 7}
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14}
+function Content (props){
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
     return(
         <>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
+        <Part part={props.parts[0]} />
+        <Part part={props.parts[1]} />
+        <Part part={props.parts[2]} />
         </>
     )
 }

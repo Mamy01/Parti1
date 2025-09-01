fb5d44fdfaefb73a5efdb9531f82abc4bb48fc47
Youmane KA <youmaneka00@gmail.com>
2025-09-01 19:03:04 +0000

    exercise 1.10

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,28 +1,19 @@
 import { useState } from "react"
-import Statistics from "./Statistics";
+import Statistics from "./Statistics"
+import Button from "./Button"
 
-function App(){
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-  const handleGood =() =>{
-    setGood(good + 1)
-  }
-  const handleNeutre =() =>{
-    setNeutral(neutral +1)
-  }
-  const handleBad =() =>{
-    setBad(bad + 1)
-  }
-  
-
-  return(
+  return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={handleGood}>Good</button>
-      <button onClick={handleNeutre}>Neutre</button>
-      <button onClick={handleBad}>Bad</button>
+      <Button onClick={() => setGood(good + 1)} text="Good" />
+      <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button onClick={() => setBad(bad + 1)} text="Bad" />
+
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
--- courseinfo/src/Statistics.jsx
+++ courseinfo/src/Statistics.jsx
@@ -1,18 +1,29 @@
+import StatisticLine from "./StatisticLine"
 
-function Statistics(props) {
-    if(props.good === 0 && props.neutral === 0 && props.bad === 0){
-       return "No feedback giving"
-    }
+function Statistics({ good, neutral, bad }) {
+  const all = good + neutral + bad
+  const average = all === 0 ? 0 : (good - bad) / all
+  const positive = all === 0 ? 0 : (good / all) * 100
 
-    return (
-        <>
-            <p>Good: {props.good}</p>
-            <p>Neutre: {props.neutral}</p>
-            <p>Bad: {props.bad}</p>
-            <p>All: {(props.good + props.neutral + props.bad)}</p>
-            <p>Average: {(props.good + props.neutral + props.bad) / 3}</p>
-            <p>Positive: {(props.good) / 100}</p>
-        </>
-    )
+  if (all === 0) {
+    return <p>No feedback given</p>
+  }
+
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <StatisticLine text="All" value={all} />
+          <StatisticLine text="Average" value={average.toFixed(2)} />
+          <StatisticLine text="Positive" value={positive.toFixed(1) + " %"} />
+        </tbody>
+      </table>
+    </div>
+  )
 }
-export default Statistics 
+
+export default Statistics

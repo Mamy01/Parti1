5373561839163c45d5647e3659d156264a6ef5c6
Youmane KA <youmaneka00@gmail.com>
2025-09-02 23:16:15 +0000

    exercise 1.12

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -3,9 +3,23 @@ import Statistics from "./Statistics"
 import Button from "./Button"
 
 function App() {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+  ]
+  const [selected, setSelected] = useState(0);
+  function voyage(){
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+  setSelected(randomIndex);
+  }
 
   return (
     <div>
@@ -13,8 +27,9 @@ function App() {
       <Button onClick={() => setGood(good + 1)} text="Good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button onClick={() => setBad(bad + 1)} text="Bad" />
-
       <Statistics good={good} neutral={neutral} bad={bad} />
+      <button onClick={voyage}>Anecdotes</button>
+      <p>{anecdotes[selected]}</p>
     </div>
   )
 }
